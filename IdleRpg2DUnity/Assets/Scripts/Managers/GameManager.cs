7ee7e5a2ded28d98d340d8b582abf32a1a39391d
Soykan <gokcellkamal@gmail.com>
2022-04-04 08:41:01 +0300

    Animations finished -Spell animations needed

--- IdleRpg2DUnity/Assets/Scripts/Managers/GameManager.cs
+++ IdleRpg2DUnity/Assets/Scripts/Managers/GameManager.cs
@@ -22,12 +22,14 @@ public class GameManager : Singleton<GameManager>
 
     public GameObject playerGo;
     
+    public Transform playerSpawnPoint;
+    
     [Header("Game Settings")]
     public float DifficultyIncreasePerLevelAsPercentage = 0.1f;
     
     private void Start()
     {
-        playerGo = Instantiate(player.prefab);
+        playerGo = Instantiate(player.prefab, playerSpawnPoint.position, Quaternion.identity);
         gameManagerStateMachine = new StateMachine();
 
         gameInitState = new GameInitState(this, gameManagerStateMachine);
